bef1c489f3aa5bdd4c95139b6831eb941eb663bb
kwmiebach <kwmiebach@gmail.com>
2014-03-30 19:18:35 +0200

    formatting for strings, not only for logging. improve logging

--- motor_03_logging/motor_03_logging.ino
+++ motor_03_logging/motor_03_logging.ino
@@ -8,9 +8,13 @@ Hardware: Arduino Uno with Arduino Motor Shield and 1 motor connected.
 
 /* CONFIG: */
 
-int onSec = 20;
-//int onSec = 60 * 3;
-int offSec = 60 * 10;
+int onSec = 60 * 3;
+int offSec = 60 * 7;
+
+/*
+int onSec = 7;
+int offSec = 11;
+*/
 
 /* CONSTANTS: */
 
@@ -23,6 +27,7 @@ int brake = 9;
 
 /* INIT: */
 
+int loop_count;
 SoftwareSerial mySerial(10, 11); // RX, TX
 
 /* HELPERS: */
@@ -31,7 +36,11 @@ void slog(char* msg) {
   Serial.print(msg);
 }
 
-void slogf(char *fmt, ... ) {
+void slogMinus() {
+  slog("---------------------------------------\n");
+}
+
+char *sformat(char *fmt, ... ) {
   // include <stdarg.h>
   // see http://playground.arduino.cc/Main/Printf
   char buf[128]; // resulting string limited to 128 chars
@@ -39,20 +48,23 @@ void slogf(char *fmt, ... ) {
   va_start (args, fmt );
   vsnprintf(buf, 128, fmt, args);
   va_end (args);
-  Serial.print(buf);
+  return buf;
 }
 
-void delaySec(int s, char* msg) {
+void delaySec(int s, char* msg, int lc) {
   div_t maxdt = div(s, 60)
   ;
   int maxm = maxdt.quot;
   int maxs = maxdt.rem;
   for (int seconds = s; seconds > 0; seconds--) {   
-    if ( (s - seconds < 6 ) || (seconds <= 15) || ( seconds % 15 == 0 )) {
+    if ( (s - seconds < 6
+    ) || (seconds <= 15) || ( seconds % 15 == 0 )) {
       div_t loopdt = div(seconds, 60);
       int loopm = loopdt.quot;
       int loops = loopdt.rem;
-      slogf("%s - delay 00:%02d:%02d / 00:%02d:%02d ... \n", msg, loopm, loops, maxm, maxs);
+      slog(sformat("Loop no. %d / currently %s for %02dm%02ds / %02dm%02ds left.\n", lc, 
+  
+        msg, maxm, maxs, loopm, loops));
     }
     delay(1000);  
   }
@@ -66,6 +78,9 @@ void setup() {
   // while (!Serial) ... // Leonardo only, wait for port to open
   
   slog("Arduino initializing...\n");
+  
+  //init loop counter for logging
+  loop_count = 0;
 
   // initialize the digital pins as outputs
   pinMode(led, OUTPUT);  // LED
@@ -77,18 +92,23 @@ void setup() {
 /* MAIN LOOP: */
 
 void loop() {
-  //forward @ full speed
+
   slog("Start channel A ...\n");
   digitalWrite(forward, HIGH); // Establishes forward direction of Channel A
   digitalWrite(brake, LOW);   // Disengage the Brake for Channel A
   analogWrite(3, 255);   // Spins the motor on Channel A at full speed
   digitalWrite(led, HIGH);   // Turn the LED on (HIGH is the voltage level)
   
-  delaySec(onSec, "started");
+  delaySec(onSec, "RUNNING", loop_count);
+  slogMinus();
+
 
   slog("Stop channel A ...\n");
   digitalWrite(brake, HIGH); // Engage the Brake for Channel A
   digitalWrite(led, LOW);    // Turn the LED off by making the voltage LOW
 
-  delaySec(offSec, "stopped");
+  delaySec(offSec, "PAUSING", loop_count);
+  slogMinus();
+  
+  loop_count++;
 }
