aaf64e32134f328fb4e4eed78f8747dcb516db56
kwmiebach <kwmiebach@gmail.com>
2014-04-18 19:44:56 +0200

    Integrate button A and B check into motor on off delay loop, only logging if pressed.

--- motor_04_switch/motor_04_switch.ino
+++ motor_04_switch/motor_04_switch.ino
@@ -18,6 +18,7 @@ int offSec = 60 * 4;
 #define BUTTON_PIN_A      2  // Pin for Button A
 #define BUTTON_PIN_B      3  // Pin for Button B
 #define DELAY            20  // Button probe - delay in ms
+#define DELAYS_PER_SEC   50  // Number of delays per second = 1000 / DELAY
 
 #define LED              13  // Pin for onboard LED
 
@@ -31,10 +32,6 @@ boolean handle_button(int pin) {
 /* VARS: */
 
 int loop_count;
-
-/* INIT: */
-
-SoftwareSerial mySerial(10, 11); // RX, TX
 
 /* HELPERS: */
 
@@ -68,7 +65,6 @@ void delaySec(int s, char* msg, int lc) {
   int coffs = coffdt.rem;
   slog(sformat("RUN: %02dm%02ds / PAUSE: %02dm%02ds\n", conm, cons, coffm, coffs));
   
-  
   for (int seconds = s; seconds > 0; seconds--) {   
     if ( (s - seconds < 6
     ) || (seconds <= 15) || ( seconds % 15 == 0 )) {
@@ -78,27 +74,83 @@ void delaySec(int s, char* msg, int lc) {
       slog(sformat("Loop no. %d / currently %s / %02dm%02ds left.\n", 
         lc, msg, loopm, loops));
     }
-    delay(1000);  
+
+    // we need to tack if there were button infos printed:
+    static int any_button;
+    any_button = 0; // no button info printed yet
+
+    static int char_count; //count single chars printed 
+    char_count = 0;
+
+    for (int delays = 0; delays < DELAYS_PER_SEC; delays++) { 
+      delay(DELAY);
+
+      // check button A:
+      boolean button_pressed_a = handle_button(BUTTON_PIN_A);
+
+      // write dot, or A if pressed for Button A:
+      if (button_pressed_a) {
+        if (any_button == 0) {
+          any_button = 1;
+          Serial.println(); //newline before first Button info char
+        }
+        Serial.print("A");
+        char_count++;
+      }
+
+      // same for B:
+      boolean button_pressed_b = handle_button(BUTTON_PIN_B);
+      if (button_pressed_b) {
+        if (any_button == 0) {
+          any_button = 1;
+          Serial.println(); //newline before first Button info char
+        }
+        Serial.print("B");
+        char_count++;
+      }
+
+      if (char_count > 0)
+        // add newline every 64 chars:
+        if ((char_count & 0x1f) == 0) //0x1f = 31, 0x3f = 63
+          Serial.println();
+    }
+
+    if (any_button == 1) {
+      //newline after Button info line(s), only if there was something:
+      Serial.println(); 
+      any_button = 0;
+    }
   }
 }
 
 /* SETUP: */
 
 void setup() {
-  // Open serial communications
+  // Open serial console:
+  SoftwareSerial mySerial(10, 11); // RX, TX
   Serial.begin(9600);
-  // while (!Serial) ... // Leonardo only, wait for port to open
-  
+ 
   slog("Arduino initializing...\n");
+  
+  // digital pin initialize A:
+  pinMode(BUTTON_PIN_A,INPUT);
+  digitalWrite(BUTTON_PIN_A, HIGH); // configure as pull-up using internal resistor
+
+  // same for B:
+  pinMode(BUTTON_PIN_B,INPUT);
+  digitalWrite(BUTTON_PIN_B, HIGH);
   
   //init loop counter for logging
   loop_count = 0;
  
   // initialize the digital pins as outputs
   pinMode(LED, OUTPUT);  // LED
+
   // Motor Channel A:
-  pinMode(FORWARE, OUTPUT); // Initiates Motor Channel A pin
+  pinMode(FORWARD, OUTPUT); // Initiates Motor Channel A pin
   pinMode(BRAKE, OUTPUT); // Initiates Brake Channel A pin 
+
+  slog("... initialized.\n");
 }
 
 /* MAIN LOOP: */
@@ -106,7 +158,7 @@ void setup() {
 void loop() {
 
   slog("Start channel A ...\n");
-  digitalWrite(FORWARE, HIGH); // Establishes forward direction of Channel A
+  digitalWrite(FORWARD, HIGH); // Establishes forward direction of Channel A
   digitalWrite(BRAKE, LOW);   // Disengage the Brake for Channel A
   analogWrite(3, SPEED);   // Spins the motor on Channel A at the desired speed
   digitalWrite(LED, HIGH);   // Turn the LED on (HIGH is the voltage level)
